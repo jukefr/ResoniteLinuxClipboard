ccef3d5d50a42490d00c1d729e3141af80582b20
Kate <code@juke.fr>
2026-04-28 17:28:52 +0200

    fix(cicd): use class-based ImageFormat with instance properties

--- stubs/Elements.Assets/Elements.Assets.cs
+++ stubs/Elements.Assets/Elements.Assets.cs
@@ -3,18 +3,18 @@ using System.IO;
 
 namespace Elements.Assets {
     public class CommonClipboard {
-        public enum ImageFormat {
-            OLE,
-            Extension
+        public class ImageFormatClass {
+            public ImageFormatClass OLE { get; }
+            public ImageFormatClass Extension { get; }
+            public string MimeType { get; }
         }
 
-        public ImageFormat ImageFormats { get; }
-        public string MimeType { get; }
+        public ImageFormatClass ImageFormats { get; }
     }
 
     public class Bitmap2D {
         public void Save(string path, object format) { }
-        public void Save(string path, CommonClipboard.ImageFormat format) { }
+        public void Save(string path, CommonClipboard.ImageFormatClass format) { }
         public static Bitmap2D Load(string path) => new Bitmap2D();
         public static Bitmap2D Load(MemoryStream stream, string extension, bool unknown) => new Bitmap2D();
     }
