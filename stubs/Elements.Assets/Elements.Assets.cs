0d280e9608f55ac40a4fbad972ae96c05d709490
Kate <code@juke.fr>
2026-04-28 17:34:23 +0200

    fix(cicd): fix ImageFormat and Bitmap2D API in stub

--- stubs/Elements.Assets/Elements.Assets.cs
+++ stubs/Elements.Assets/Elements.Assets.cs
@@ -1,20 +1,20 @@
 using System;
+using System.Collections.Immutable;
 using System.IO;
 
 namespace Elements.Assets {
     public class CommonClipboard {
         public class ImageFormat {
-            public ImageFormat OLE { get; }
-            public ImageFormat Extension { get; }
-            public string MimeType { get; }
+            public ImageFormat OLE { get; } = new ImageFormat();
+            public ImageFormat Extension { get; } = new ImageFormat();
+            public string MimeType { get; } = "";
         }
 
-        public ImageFormat ImageFormats { get; }
+        public ImmutableArray<ImageFormat> ImageFormats { get; }
     }
 
     public class Bitmap2D {
-        public void Save(Stream stream, object format) { }
-        public void Save(Stream stream, string format) { }
+        public void Save(object streamOrPath, object format) { }
         public static Bitmap2D Load(Stream stream, string extension, bool unknown) => new Bitmap2D();
         public static Bitmap2D Load(string path) => new Bitmap2D();
     }
