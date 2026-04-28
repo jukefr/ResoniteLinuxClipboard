b2301d5584eb4942473b08eca819856f196c188a
Kate <code@juke.fr>
2026-04-28 17:31:41 +0200

    fix(cicd): fix Bitmap2D.Save to accept Stream

--- stubs/Elements.Assets/Elements.Assets.cs
+++ stubs/Elements.Assets/Elements.Assets.cs
@@ -3,19 +3,19 @@ using System.IO;
 
 namespace Elements.Assets {
     public class CommonClipboard {
-        public class ImageFormatClass {
-            public ImageFormatClass OLE { get; }
-            public ImageFormatClass Extension { get; }
+        public class ImageFormat {
+            public ImageFormat OLE { get; }
+            public ImageFormat Extension { get; }
             public string MimeType { get; }
         }
 
-        public ImageFormatClass ImageFormats { get; }
+        public ImageFormat ImageFormats { get; }
     }
 
     public class Bitmap2D {
-        public void Save(string path, object format) { }
-        public void Save(string path, CommonClipboard.ImageFormatClass format) { }
+        public void Save(Stream stream, object format) { }
+        public void Save(Stream stream, string format) { }
+        public static Bitmap2D Load(Stream stream, string extension, bool unknown) => new Bitmap2D();
         public static Bitmap2D Load(string path) => new Bitmap2D();
-        public static Bitmap2D Load(MemoryStream stream, string extension, bool unknown) => new Bitmap2D();
     }
 }
